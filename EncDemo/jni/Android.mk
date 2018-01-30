42186f5bcee6c2849ff5aa02d0cfa587bc80b0c6
Vi <954116843@qq.com>
2018-01-30 10:23:36 +0800

    Update Android.mk

--- EncDemo/jni/Android.mk
+++ EncDemo/jni/Android.mk
@@ -5,7 +5,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE    := EncDemo
 LOCAL_SRC_FILES := EncDemo.cpp
 LOCAL_C_INCLUDES :=  $(LOCAL_PATH) $(LOCAL_PATH)/..  
-LOCAL_LDLIBS := -L$(SYSROOT)/usr/lib -llog -fPIC -Wl,-init=ijiami
+LOCAL_LDLIBS := -L$(SYSROOT)/usr/lib -llog -fPIC -Wl,-init=hellojni
 LOCAL_STATIC_LIBRARIES := libzlib  
 
 include $(BUILD_SHARED_LIBRARY)
